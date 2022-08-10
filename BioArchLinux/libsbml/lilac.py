3f0864d75cbec65eeabbc86933b1f9f4fd79fd0c
sukanka <su975853527@gmail.com>
2022-08-10 21:42:05 +0800

    reimplement 5f218cc

--- BioArchLinux/libsbml/lilac.py
+++ BioArchLinux/libsbml/lilac.py
@@ -1,35 +1,19 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-
-def apply_patch(filename, patch,reverse=False):
-    rev="-R" if reverse else ""
-    patch_proc = subprocess.Popen(
-        ["patch", "-p1", rev, filename], stdin=subprocess.PIPE, text=True)
-    patch_proc.communicate(patch)
-
+# for our server, we have to use make -j6, but it should not appears in the PKGBUILD
 def pre_build():
     update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
     run_cmd(['updpkgsums'])
-    apply_patch('PKGBUILD', PATCH)
+    for line in edit_file('PKGBUILD'):
+        if line.strip() == 'make':
+            line = line + ' -j6'
+        print(line)
 
 def post_build():
-    apply_patch('PKGBUILD', PATCH, reverse=True)
+    for line in edit_file('PKGBUILD'):
+        if line.endswith('make -j6'):
+            line = line[:-4]
+        print(line)
     git_add_files('PKGBUILD')
     git_commit()
-
-PATCH = r"""
-diff --git a/BioArchLinux/libsbml/PKGBUILD b/BioArchLinux/libsbml/PKGBUILD
-index 2cf9189a0..e19634ec2 100644
---- a/BioArchLinux/libsbml/PKGBUILD
-+++ b/BioArchLinux/libsbml/PKGBUILD
-@@ -34,7 +34,7 @@ build() {
-     -DENABLE_FBC=ON \
-     -DENABLE_GROUPS=ON
-
--  make
-+  make -j6
- }
-
- package() {
-"""
