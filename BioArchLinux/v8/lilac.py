77244bae2a7ff2e9e439d2c246cb472783334bd4
sukanka <su975853527@gmail.com>
2022-08-18 20:38:40 +0800

    v8: use 6 threads

--- BioArchLinux/v8/lilac.py
+++ BioArchLinux/v8/lilac.py
@@ -4,7 +4,11 @@ from lilaclib import *
 def pre_build():
     update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
     run_cmd(['updpkgsums'])
+    # run 6 jobs in parallel on our server.
+    run_cmd(['sh', '-c', "sed -i 's|ninja|ninja -j6|g' PKGBUILD"])
 
 def post_build():
+    # don't fix threads in PKGBUILD.
+    run_cmd(['sh', '-c', "sed -i 's|ninja -j6|ninja|g' PKGBUILD"])
     git_add_files('PKGBUILD')
     git_commit()
