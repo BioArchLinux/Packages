b9c1982432fbfe60037a42391a015543c6e5f1da
sukanka <su975853527@gmail.com>
2022-11-15 23:55:03 +0800

    jasp-desktop: follow upstream, use ninja

--- BioArchLinux/jasp-desktop/lilac.py
+++ BioArchLinux/jasp-desktop/lilac.py
@@ -2,16 +2,18 @@
 from lilaclib import *
 
 # we use 5 threads in BioArchLinux, but it should not be pushed to aur.
+
 def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('_pkgver='):
             line = f'_pkgver={_G.newver}'
-        elif line.startswith('build()'):
-            line += '\n' + 'export MAKE="make -j5"'
+        elif line.strip() == 'ninja':
+            line += ' -j5'
         print(line)
     update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
 
+
 def post_build():
-    run_cmd(['sh', '-c', "sed -i '/export MAKE/d' PKGBUILD"])
+    run_cmd(['sh', '-c', "sed -i '/ninja -j5/ninja/d' PKGBUILD"])
     git_pkgbuild_commit()
     update_aur_repo()
