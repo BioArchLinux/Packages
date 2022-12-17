d663090ea47cbbe376bccd06b4caf11a35052f20
sukanka <su975853527@gmail.com>
2022-12-17 13:32:37 +0800

    fix depends

---
+++ BioArchLinux/r-spdl/lilac.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+def pre_build():
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('_pkgver='):
+            line = f'_pkgver={_G.newver}'
+        print(line)
+    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
