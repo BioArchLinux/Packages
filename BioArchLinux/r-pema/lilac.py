5cbe9a3d6e7cc2aa1c3613496b0486b728d1c45b
sukanka <su975853527@gmail.com>
2023-01-27 15:20:51 +0800

    r-pema: set threads by export and rebuild

--- BioArchLinux/r-pema/lilac.py
+++ BioArchLinux/r-pema/lilac.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 from lilaclib import *
-from os import environ
+
+# we use 5 threads in BioArchLinux, but it should not be pushed to aur.
 
 def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('_pkgver='):
             line = f'_pkgver={_G.newver}'
+        elif line.startswith('build()'):
+            line += '\n' + 'export MAKE="make -j5"'
         print(line)
     update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
-    # each thread consumes about 2GiB memory
-    environ.setdefault("MAKE", "make -j5")
+
+
+def post_build():
+    run_cmd(['sh', '-c', "sed -i '/export MAKE/d' PKGBUILD"])
+    git_pkgbuild_commit()
+    update_aur_repo()
