8c284fb2f6416b2a34f28770ac585fe072614108
sukanka <su975853527@gmail.com>
2023-04-24 21:53:04 +0800

    r-rstanarm: use 2 threads to prevent OOM

--- BioArchLinux/r-rstanarm/lilac.py
+++ BioArchLinux/r-rstanarm/lilac.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-# we use 5 threads in BioArchLinux, but it should not be pushed to aur.
+# we use 2 threads in BioArchLinux, but it should not be pushed to aur.
 def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('_pkgver='):
             line = f'_pkgver={_G.newver}'
         elif line.startswith('build()'):
-            line += '\n' + 'export MAKE="make -j5"'
+            line += '\n' + 'export MAKE="make -j2"'
         print(line)
     update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
 
