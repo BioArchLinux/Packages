8c64f150e9e858ec51b75f8318216efefe30b0b4
Pekka Ristola <pekkarr@protonmail.com>
2023-07-01 16:52:50 +0300

    r-sodium: init, checkdepends for r-gargle

---
+++ BioArchLinux/r-sodium/lilac.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
+def pre_build():
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "libsodium (>= 1.0.3)",
+    )
+
+def post_build():
+    git_pkgbuild_commit()
