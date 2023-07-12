2e6526d0e1dd4b6a949e0ca553d4670f33e9e731
Pekka Ristola <pekkarr@protonmail.com>
2023-07-13 00:02:05 +0300

    r-microbiomestat: init, dependency of r-benchdamic

---
+++ BioArchLinux/r-microbiomestat/lilac.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
+def pre_build():
+    r_pre_build(
+        _G,
+        expect_needscompilation = True,
+    )
+
+def post_build():
+    git_pkgbuild_commit()
+    update_aur_repo()
