615c1d6c4ef9d400c9047f0f486c18cebaa86106
Pekka Ristola <pekkarr@protonmail.com>
2023-08-05 16:06:25 +0300

    r-v8: use metadata checks

--- BioArchLinux/r-v8/lilac.py
+++ BioArchLinux/r-v8/lilac.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_pkgver='):
-            line = f'_pkgver={_G.newver}'
-        print(line)
-    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "V8 engine version 6+ is needed for ES6 and WASM support. On Linux you can build against libv8-dev (Debian) or v8-devel (Fedora). We also provide static libv8 binaries for most platforms, see the README for details.",
+    )
 
 def post_build():
     git_pkgbuild_commit()
