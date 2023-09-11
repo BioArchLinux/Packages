c2d70ea0dc66de8895e54a5c45f7d1cea1e4fa71
Pekka Ristola <pekkarr@protonmail.com>
2023-09-11 20:09:44 +0300

    r-rpostgres: init, checkdependency of r-sf

---
+++ BioArchLinux/r-rpostgres/lilac.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
+def pre_build():
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "libpq >= 9.0: libpq-dev (deb) or postgresql-devel (rpm)",
+    )
+
+def post_build():
+    git_pkgbuild_commit()
