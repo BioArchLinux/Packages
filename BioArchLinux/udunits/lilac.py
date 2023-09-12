2d06c3a43140bce498669a5710c48fe8e0e51502
Pekka Ristola <pekkarr@protonmail.com>
2023-09-12 14:52:43 +0300

    udunits: improve PKGBUILD

--- BioArchLinux/udunits/lilac.py
+++ BioArchLinux/udunits/lilac.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
-
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+    update_pkgver_and_pkgrel(_G.newver)
+    run_cmd(['updpkgsums'])
 
 def post_build():
-  git_pkgbuild_commit()
-# vim:set ts=2 sw=2 et:
-
+    git_pkgbuild_commit()
