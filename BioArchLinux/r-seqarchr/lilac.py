0f856b7b7b23ee79428d847474a18e7eedcbc75b
Pekka Ristola <pekkarr@protonmail.com>
2023-11-14 13:32:43 +0200

    r-seqarchr: init, dependency of r-seqarchrplus

---
+++ BioArchLinux/r-seqarchr/lilac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
+def pre_build():
+    r_pre_build(
+        _G,
+        expect_license = "GPL-3 | file LICENSE",
+        expect_systemrequirements = "Python (>= 3.5), scikit-learn (>= 0.21.2), packaging",
+    )
+
+def post_build():
+    git_pkgbuild_commit()
+    update_aur_repo()
