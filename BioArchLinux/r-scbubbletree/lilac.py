5a6b3e86486f4d89adcc250b55ce532feaf4db58
Pekka Ristola <pekkarr@protonmail.com>
2023-12-05 13:30:36 +0200

    r-scbubbletree: init

---
+++ BioArchLinux/r-scbubbletree/lilac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
+def pre_build():
+    r_pre_build(
+        _G,
+        expect_license = "GPL-3 + file LICENSE",
+        expect_systemrequirements = "Python (>= 3.6), leidenalg (>= 0.8.2)",
+    )
+
+def post_build():
+    git_pkgbuild_commit()
+    update_aur_repo()
