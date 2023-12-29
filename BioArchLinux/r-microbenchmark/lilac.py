ececeb788a23007e718e61b5a0f1af387f3d38d5
Pekka Ristola <pekkarr@protonmail.com>
2023-12-29 13:30:05 +0200

    r-*: use metadata checks for several packages

--- BioArchLinux/r-microbenchmark/lilac.py
+++ BioArchLinux/r-microbenchmark/lilac.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_pkgver='):
-            line = f'_pkgver={_G.newver}'
-        print(line)
-    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "On a Unix-alike, one of the C functions mach_absolute_time (macOS), clock_gettime or gethrtime. If none of these is found, the obsolescent POSIX function gettimeofday will be tried.",
+    )
 
 def post_build():
     git_pkgbuild_commit()
