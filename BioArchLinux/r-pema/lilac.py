22cd961e99e3fe5b5777b520d5ce092da33a3ada
Pekka Ristola <pekkarr@protonmail.com>
2024-01-02 18:26:27 +0200

    r-pema: use metadata checks

--- BioArchLinux/r-pema/lilac.py
+++ BioArchLinux/r-pema/lilac.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-# we use 5 threads in BioArchLinux, but it should not be pushed to aur.
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
 
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_pkgver='):
-            line = f'_pkgver={_G.newver}'
-        elif line.startswith('build()'):
-            line += '\n' + 'export MAKE="make -j2"'
-        print(line)
-    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
-
+    r_pre_build(_G)
 
 def post_build():
-    run_cmd(['sh', '-c', "sed -i '/export MAKE/d' PKGBUILD"])
     git_pkgbuild_commit()
     update_aur_repo()
