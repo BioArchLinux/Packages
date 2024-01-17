d923172864c520fc99fbefc55cb945951d8cda2b
Pekka Ristola <pekkarr@protonmail.com>
2024-01-17 20:53:28 +0200

    r-*: use SPDX license identifier for "GPL-2 | GPL-3"

--- lilac-extensions/lilac_r_utils.py
+++ lilac-extensions/lilac_r_utils.py
@@ -206,7 +206,7 @@ license_map = {
     "GPL (>= 3.0)": "GPL3",
     "GPL (>=2)": "GPL-2.0-or-later",
     "GPL-2": "GPL2",
-    "GPL-2 | GPL-3": "GPL",
+    "GPL-2 | GPL-3": "GPL-2.0-only OR GPL-3.0-only",
     "GPL-3": "GPL3",
     "LGPL": "LGPL-2.0-or-later",
     "LGPL (>= 2)": "LGPL",
