6293732ce6f37ddaed00eb7ba28a1dc853781738
Pekka Ristola <pekkarr@protonmail.com>
2024-03-09 13:44:50 +0200

    r-*: use metadata checks for several packages

--- BioArchLinux/r-clipr/lilac.py
+++ BioArchLinux/r-clipr/lilac.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_pkgver='):
-            line = f'_pkgver={_G.newver}'
-        print(line)
-    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "xclip (https://github.com/astrand/xclip) or xsel (http://www.vergenet.net/~conrad/software/xsel/) for accessing the X11 clipboard, or wl-clipboard (https://github.com/bugaevc/wl-clipboard) for systems using Wayland.",
+    )
 
 def post_build():
     git_pkgbuild_commit()
