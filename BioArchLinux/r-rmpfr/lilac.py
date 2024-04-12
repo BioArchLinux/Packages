eeb1f42517f9a4214ce1e3b7e3fb09a3bd01b757
Pekka Ristola <pekkarr@protonmail.com>
2024-04-12 14:03:06 +0300

    r-*: use metadata checks for several packages

--- BioArchLinux/r-rmpfr/lilac.py
+++ BioArchLinux/r-rmpfr/lilac.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_pkgver='):
-            line = f'_pkgver={_G.newver}'
-        print(line)
-    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "gmp (>= 4.2.3), mpfr (>= 3.0.0), pdfcrop (part of TexLive) is required to rebuild the vignettes.",
+    )
 
 def post_build():
     git_pkgbuild_commit()
