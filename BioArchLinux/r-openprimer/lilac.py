969f5b31c8d0f400d2917c223dc49aec7b6dc0cd
Pekka Ristola <pekkarr@protonmail.com>
2024-05-04 11:00:18 +0300

    r-openprimer: fix depends, use metadata checks

--- BioArchLinux/r-openprimer/lilac.py
+++ BioArchLinux/r-openprimer/lilac.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_pkgver='):
-            line = f'_pkgver={_G.newver}'
-        print(line)
-    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "MAFFT (>= 7.305), OligoArrayAux (>= 3.8), ViennaRNA (>= 2.4.1), MELTING (>= 5.1.1), Pandoc (>= 1.12.3)",
+    )
 
 def post_build():
     git_pkgbuild_commit()
