b2de7f1c80f8e25de87693e9b3a8e4c75f95e023
Pekka Ristola <pekkarr@protonmail.com>
2024-05-07 14:48:02 +0300

    r-*: fix depends, use metadata checks for several packages

--- BioArchLinux/r-hireadsprocessor/lilac.py
+++ BioArchLinux/r-hireadsprocessor/lilac.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_pkgver='):
-            line = f'_pkgver={_G.newver}'
-        print(line)
-    update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "BLAT, UCSC hg18 in 2bit format for BLAT",
+    )
 
 def post_build():
     git_pkgbuild_commit()
