a1d3d9698be738049ce0adf2a445de83a7c0ed42
Pekka Ristola <pekkarr@protonmail.com>
2024-07-25 13:32:28 +0300

    r-websocket: fix systemrequirements

--- BioArchLinux/r-websocket/lilac.py
+++ BioArchLinux/r-websocket/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_systemrequirements = "C++11, GNU make, OpenSSL >= 1.0.2",
+        expect_systemrequirements = "GNU make, OpenSSL >= 1.0.2",
     )
 
 def post_build():
