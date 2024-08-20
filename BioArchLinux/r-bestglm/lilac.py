c3dbab26daaabeb5fdf7502990ceaceefe52b4f0
Pekka Ristola <pekkarr@protonmail.com>
2024-08-20 14:44:16 +0300

    r-*: fix recently added R packages

--- BioArchLinux/r-bestglm/lilac.py
+++ BioArchLinux/r-bestglm/lilac.py
@@ -7,7 +7,10 @@ sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
 from lilac_r_utils import r_pre_build
 
 def pre_build():
-    r_pre_build(_G)
+    r_pre_build(
+        _G,
+        expect_needscompilation = True,
+    )
 
 def post_build():
     git_pkgbuild_commit()
