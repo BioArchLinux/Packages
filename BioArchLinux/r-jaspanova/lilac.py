87f6d9237ce38661c8aa07b88882e7640876f68f
sukanka <su975853527@gmail.com>
2024-09-17 17:12:14 +0800

    r-bas: add makedepends, r-jaspanova: use old fashioned lilac.py

--- BioArchLinux/r-jaspanova/lilac.py
+++ BioArchLinux/r-jaspanova/lilac.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-import os
-import sys
-
-sys.path.append(os.path.normpath(f"{__file__}/../../../lilac-extensions"))
-from lilac_r_utils import r_pre_build
-
 
 def pre_build():
-    r_pre_build(_G)
+    for line in edit_file("PKGBUILD"):
+        if line.startswith("_pkgver="):
+            line = f"_pkgver={_G.newver}"
+        print(line)
+    update_pkgver_and_pkgrel(_G.newver.replace(":", ".").replace("-", "."))
