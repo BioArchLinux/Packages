60304b774866bb6b6ceef1d91af0996b570667b9
Pekka Ristola <pekkarr@protonmail.com>
2024-10-31 12:47:47 +0200

    r-git2r: fix systemrequirements

--- BioArchLinux/r-git2r/lilac.py
+++ BioArchLinux/r-git2r/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_systemrequirements = "By default, git2r uses a system installation of the libgit2 headers and library. However, if a system installation is not available, builds and uses a bundled version of the libgit2 source. zlib headers and library. OpenSSL headers and library (non-macOS). LibSSH2 (optional on non-Windows) to enable the SSH transport.",
+        expect_systemrequirements = "libgit2 (>= 1.0): libgit2-devel (rpm) or libgit2-dev (deb)",
     )
 
 def post_build():
