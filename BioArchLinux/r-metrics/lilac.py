ef7decd7264e405d9ceacec440b64ab52558de01
Shun Wang <shuonwang@gmail.com>
2024-12-25 14:26:18 +0800

    r-metrics: fix missing licence files

--- BioArchLinux/r-metrics/lilac.py
+++ BioArchLinux/r-metrics/lilac.py
@@ -7,7 +7,10 @@ sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
 from lilac_r_utils import r_pre_build
 
 def pre_build():
-    r_pre_build(_G)
+    r_pre_build(
+        _G,
+        expect_license = "BSD_3_clause + file LICENCE",
+    )
 
 def post_build():
     git_pkgbuild_commit()
