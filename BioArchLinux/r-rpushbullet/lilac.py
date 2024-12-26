89a2daeb0d7c5fb59434a444f7663aaa830731cb
Shun Wang <shuonwang@gmail.com>
2024-12-26 11:54:50 +0800

    r-rpushbullet: change expect_systemrequirements

--- BioArchLinux/r-rpushbullet/lilac.py
+++ BioArchLinux/r-rpushbullet/lilac.py
@@ -7,7 +7,10 @@ sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
 from lilac_r_utils import r_pre_build
 
 def pre_build():
-    r_pre_build(_G)
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "A user API key (which one can request from the website at <http://www.pushbullet.com>), and one or more devices to push messages to which may be any one of an (Android or iOS) phone, a (Chrome or Firefox, or Opera or Safari) browser or the (Windows or Mac) desktop application provided the corresponding Pushbullet 'app' has been installed on any one of these.",
+    )
 
 def post_build():
     git_pkgbuild_commit()
