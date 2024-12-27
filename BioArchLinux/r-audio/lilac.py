5de038b1e2fb3b02f3adbd724866a43d9ecea523
Shun Wang <shuonwang@gmail.com>
2024-12-27 09:45:35 +0800

    r-audio: fix typo in license

--- BioArchLinux/r-audio/lilac.py
+++ BioArchLinux/r-audio/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_license = "MIT + file LICENCE",
+        expect_license = "MIT + file LICENSE",
     )
 
 def post_build():
