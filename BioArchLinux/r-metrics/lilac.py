60d6d8d093b401dd7f535f6bab674079fb4e9979
Shun Wang <shuonwang@gmail.com>
2025-01-02 10:19:14 +0800

    r-metrics: fix to license

--- BioArchLinux/r-metrics/lilac.py
+++ BioArchLinux/r-metrics/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_license = "BSD_3_clause + file LICENCE",
+        expect_license = "BSD_3_clause + file LICENSE",
     )
 
 def post_build():
