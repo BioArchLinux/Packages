820610f6d9a19819b396aa6bfa5890916c251a35
Kuoi <kuoi@bioarchlinux.org>
2025-04-18 22:14:55 +1000

    r-rgraphviz: fix the reuqirement

--- BioArchLinux/r-rgraphviz/lilac.py
+++ BioArchLinux/r-rgraphviz/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_systemrequirements = "optionally Graphviz (>= 2.16)",
+        expect_systemrequirements = "optionally Graphviz (>= 2.16), USE_C17",
     )
 
 def post_build():
