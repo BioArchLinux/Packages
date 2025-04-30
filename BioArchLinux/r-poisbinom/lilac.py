b3095cf5493700da2741dd5bf92413fc6613c269
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-04-30 13:44:56 +1000

    r-poisbinom: fix sysreq

--- BioArchLinux/r-poisbinom/lilac.py
+++ BioArchLinux/r-poisbinom/lilac.py
@@ -7,7 +7,8 @@ sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
 from lilac_r_utils import r_pre_build
 
 def pre_build():
-    r_pre_build(_G)
+    r_pre_build(_G,
+            expect_systemrequirements = "fftw (>= 3)",)
 
 def post_build():
     git_pkgbuild_commit()
