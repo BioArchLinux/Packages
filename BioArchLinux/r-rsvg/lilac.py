336d111e7fa12b95050861a5ea1f67c2a3728474
Shun Wang <shuonwang@gmail.com>
2025-09-15 23:23:11 +0800

    r-rsvg: fix system requirement

--- BioArchLinux/r-rsvg/lilac.py
+++ BioArchLinux/r-rsvg/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_systemrequirements = "librsvg2",
+        expect_systemrequirements = "librsvg2 (at least version 2.52)",
     )
 
 def post_build():
