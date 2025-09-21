d05d05bd7ab2b545e81e6b8588b77ce6ed11362a
sukanka <su975853527@gmail.com>
2025-09-21 11:23:24 +0800

    r-fmesher: skip r_pre_build

--- BioArchLinux/r-fmesher/lilac.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import os
-import sys
-sys.path.append(os.path.normpath(f"{__file__}/../../../lilac-extensions"))
-from lilac_r_utils import r_pre_build
-
-def pre_build():
-    r_pre_build(_G)
