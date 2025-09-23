a821b619e006374e047a7a2284476e487540bade
Shun Wang <shuonwang@gmail.com>
2025-09-23 19:39:41 +0800

    r-ragg: fix system requirements

--- BioArchLinux/r-ragg/lilac.py
+++ BioArchLinux/r-ragg/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_systemrequirements = "freetype2, libpng, libtiff, libjpeg",
+        expect_systemrequirements = "freetype2, libpng, libtiff, libjpeg, libwebp, libwebpmux",
     )
 
 def post_build():
