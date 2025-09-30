fe916fe67e9d15df997e1a77c2122bfd5f8a3e3a
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-09-30 14:48:02 +1000

    r-admixr: fix lilac

--- BioArchLinux/r-admixr/lilac.py
+++ BioArchLinux/r-admixr/lilac.py
@@ -7,7 +7,11 @@ sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
 from lilac_r_utils import r_pre_build
 
 def pre_build():
-    r_pre_build(_G)
+    r_pre_build(
+        _G,
+        expect_systemrequirements = "ADMIXTOOLS suite of command-line utilities for population genetics. See <https://reich.hms.harvard.edu/software> for the most recent installation instructions and further information."
+    )
+
 
 def post_build():
     git_pkgbuild_commit()
