ec1ac87e617d284b752062d27c1e028aa84c476b
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-11-07 12:10:07 +1100

    r-otel: init

--- BioArchLinux/r-otel/lilac.py
+++ BioArchLinux/r-otel/lilac.py
@@ -7,11 +7,7 @@ sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
 from lilac_r_utils import r_pre_build
 
 def pre_build():
-    r_pre_build(
-        _G,
-        expect_license = "MIT + file LICENSE",
-    )
+    r_pre_build(_G)
 
 def post_build():
     git_pkgbuild_commit()
-    update_aur_repo()
