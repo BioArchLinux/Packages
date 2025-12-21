b135fc2f0f1974962558be045e4f904fb73b4b1e
sukanka <su975853527@gmail.com>
2025-12-21 23:52:28 +0800

    r-hmeasure: don't use lilac_r_utils.
    archieved packages do not apply

--- BioArchLinux/r-hmeasure/lilac.py
+++ BioArchLinux/r-hmeasure/lilac.py
@@ -3,8 +3,7 @@ from lilaclib import *
 
 import os
 import sys
-sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
-from lilac_r_utils import r_pre_build
+
 
 def pre_build():
     r_pre_build(_G)
