0ce27652ff54d2bf6ace5e831cd4ac0076c0be7a
Shun Wang <shuonwang@gmail.com>
2026-04-02 18:23:00 +0800

    r-qs2: fix make dependencies

--- BioArchLinux/r-qs2/lilac.py
+++ BioArchLinux/r-qs2/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_systemrequirements = "GNU make",
+        expect_systemrequirements = "None",
     )
 
 def post_build():
