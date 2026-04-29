6f293ca473868e50d054ecfeeaaf6e84b7378d46
Guoyi Zhang <kuoi@bioarchlinux.org>
2026-04-29 12:15:04 +1000

    r-mvtnorm: try to avoid checkdepends

--- BioArchLinux/r-mvtnorm/lilac.py
+++ BioArchLinux/r-mvtnorm/lilac.py
@@ -4,7 +4,34 @@ from lilaclib import *
 import os
 import sys
 sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
-from lilac_r_utils import r_pre_build
+from lilac_r_utils import (
+    r_pre_build, 
+    all_checks, 
+    check_default_pkgs,
+    check_makedepends,
+    check_optdepends,
+    check_fortran,
+    check_systemrequirements,
+    check_license,
+    check_pkgdesc,
+    check_arch,
+    check_md5sum,
+)
+import lilac_r_utils
+
+# remove check_depends
+lilac_r_utils.all_checks = [
+    check_default_pkgs,
+    # check_depends
+    check_makedepends,
+    check_optdepends,
+    check_fortran,
+    check_systemrequirements,
+    check_license,
+    check_pkgdesc,
+    check_arch,
+    check_md5sum,
+]
 
 def pre_build():
     r_pre_build(_G)
