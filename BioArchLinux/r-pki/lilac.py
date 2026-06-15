d92772bc8a074c590a8430859fb05e949c44c3c1
Boris Shor <bshor@users.noreply.github.com>
2026-06-15 02:57:32 -0500

    r-pki: init pkg

---
+++ BioArchLinux/r-pki/lilac.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+import os
+import sys
+sys.path.append(os.path.normpath(f'{__file__}/../../../lilac-extensions'))
+from lilac_r_utils import r_pre_build
+
+def pre_build():
+    r_pre_build(
+        _G,
+        expect_license = "GPL-2 | GPL-3 | file LICENSE",
+        expect_systemrequirements = "OpenSSL library and headers (openssl-dev or\nsimilar)",
+    )
+
+def post_build():
+    git_pkgbuild_commit()
