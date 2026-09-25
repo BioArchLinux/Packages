833e70528590149889adedface36e5a4cd72bf9a
sukanka <su975853527@gmail.com>
2026-09-25 20:46:09 +0800

    r-admisc: fix system requirements and update to 0.41

--- BioArchLinux/r-admisc/lilac.py
+++ BioArchLinux/r-admisc/lilac.py
@@ -9,7 +9,7 @@ from lilac_r_utils import r_pre_build
 def pre_build():
     r_pre_build(
         _G,
-        expect_systemrequirements = "OpenMP (optional)",
+        expect_systemrequirements = "POSIX threads",
     )
 
 def post_build():
